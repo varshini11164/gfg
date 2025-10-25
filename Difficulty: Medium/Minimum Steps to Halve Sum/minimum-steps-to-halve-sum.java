20922a93cb20119a79d390b358820c976c8d09cf
varshini11164 <chvarshini1116@gmail.com>
2025-10-25 11:10:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Steps to Halve Sum/minimum-steps-to-halve-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minOperations(int[] arr) {
+        // code here
+        PriorityQueue<Double> maxPq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        double sum = 0;
+        
+        for(int i = 0 ; i < arr.length ; i++){
+            sum += arr[i];
+            maxPq.add((double)arr[i]);
+        }
+        double half = sum / 2;
+        int minOperation = 0;
+        
+        while(sum > half){
+            double maxVal = maxPq.poll();
+            double maxValHalf = maxVal / 2;
+            sum -= maxValHalf;
+            maxPq.add(maxValHalf);
+            minOperation++;
+        }
+        return minOperation;
+    }
+}
