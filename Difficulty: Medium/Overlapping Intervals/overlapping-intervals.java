fe7cec814a41fe4ca25f9c81af872450a8dd9584
varshini11164 <chvarshini1116@gmail.com>
2026-01-02 12:15:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.java
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.java
@@ -1,43 +1,27 @@
 class Solution {
     public ArrayList<int[]> mergeOverlap(int[][] arr) {
         // Code here
-    //sort the array based on the first indexes
-    Arrays.sort(arr,(a,b)->a[0]-b[0]);
-    
-    ArrayList<int[]> answer = new ArrayList<>();
-    if(arr.length==0)
-    return answer;
-    
-    int[]firstElement = new int[2];
-    firstElement[0]=arr[0][0];
-    firstElement[1]=arr[0][1];
-    int length=0;
-    answer.add(firstElement);
-   
-    int i=1;
-    while(i<arr.length)
-    {
-        int []temp=answer.get(length);
-      if(temp[1]>=arr[i][0])
-      {
-          int max=Math.max(temp[1],arr[i][1]);
-          int[]newElement =  new int[2];
-          newElement[0]=temp[0];
-          newElement[1]=max;
-          answer.set(length,newElement);
-          
-      }
-      else{
-         int[]newElement =  new int[2];
-          newElement[0]=arr[i][0];
-          newElement[1]=arr[i][1]; 
-          answer.add(newElement);
-          length++;
-      }
-     i++;   
-    }
-    
-    return answer;
+        Arrays.sort(arr,new Comparator<int[]>(){
+            public int compare(int[] arr1,int[] arr2){
+                return Integer.compare(arr1[0],arr2[0]);
+            }
+        });
+        ArrayList<int[]> result=new ArrayList<>();
+        int n=arr.length;
+        int i=0;
+        int j=1;
+        while(j<n){
+            if(arr[j][0]<=arr[i][1]){
+                arr[i][1]=Math.max(arr[i][1],arr[j][1]);
+                j++;
+            }
+            else{
+                result.add(arr[i]);
+                i=j;
+                j++;
+            }
+        }
+        result.add(arr[i]);
+        return result;
     }
 }
-
